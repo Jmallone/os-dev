84ec0c0a9dba01d40184ada2cc31c7e49ed14c61
Michel G. Souza <Michelgomesdes@hotmail.com>
2020-11-17 00:52:57 -0300

    using ld program

---
+++ pt7/Sector 1/compilar.sh
@@ -0,0 +1,11 @@
+nasm bootloader.asm -f bin -o bootloader.bin
+
+nasm ../Sector\ 2+/ExtendedProgram.asm -f elf64 -o ExtendedProgram.o
+
+gcc -Ttext 0x8000 -ffreestanding -mno-red-zone -m64 -c "Kernel.cpp" -o "Kernel.o"
+
+ld -T"link.ld" 
+
+cat bootloader.bin kernel.bin > bootloader.flp
+
+qemu-system-x86_64 -fda bootloader.flp -d int
