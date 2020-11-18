abcc0b5db1ec870d9b7af10b0c712bbe0f722464
Michel G. Souza <Michelgomesdes@hotmail.com>
2020-11-18 18:00:45 -0300

    Add some features
    now you can write stuff in the screen :D

---
+++ pt13/Sector 1/IDT.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "TypeDefs.cpp"
+#include "KBScanCodeSet1.cpp"
+
+/*Interrupt descriptor Table*/
+struct IDT64{
+	uint_16 offset_low;
+	uint_16 selector;
+	uint_8 ist;
+	uint_8 types_attr;
+	uint_16 offset_mid;
+	uint_32 offset_high;
+	uint_32 zero;
+};
+extern IDT64 _idt[256];
+extern uint_64 isr1; 
+extern "C" void LoadIDT();
+
+void InitializeIDT(){
+
+	_idt[1].zero = 0;
+	_idt[1].offset_low = (uint_16)(((uint_64)&isr1 & 0x000000000000ffff));
+	_idt[1].offset_mid = (uint_16)(((uint_64)&isr1 & 0x00000000ffff0000) >> 16);
+	_idt[1].offset_high = (uint_32)(((uint_64)&isr1 & 0xffffffff00000000) >> 32);
+	_idt[1].ist = 0;
+	_idt[1].selector = 0x08;
+	_idt[1].types_attr = 0x8e;
+
+	RemapPic();
+	outb(0x21, 0xfd);
+	outb(0xa1, 0xff);
+	LoadIDT();
+}
+
+extern "C" void isr1_handler(){
+	uint_8 scanCode = inb(0x60);
+	
+	if (scanCode < 0x3A){
+		PrintChar(KBSet1::ScanCodeLookupTable[scanCode]);
+	}
+
+	//PrintString(HexToString(inb(0x60)));
+	outb(0x20, 0x20);
+	outb(0xa0, 0x20);
+}
---
+++ pt13/Sector 1/Kernel.cpp
@@ -0,0 +1,19 @@
+#include "TextPrint.cpp"
+#include "IDT.cpp"
+
+extern const char Test[];
+extern const char Test2[];
+
+extern "C" void _start(){
+
+	ClearScreen();
+	SetCursorPosition(PositionFromCoords(0, 0));
+	PrintString("          Blue Dog OS\n\r", BACKGROUND_BLINKINGBLUE | FOREGROUND_RED);
+	PrintString(Test);
+	PrintString(Test2);
+	PrintString("\n\r");
+	InitializeIDT();
+
+
+	return;	
+}
